3588d0c16a4b1d4b97163e34431393e1ab1f38c0
Amirmahdy Pourvahaby <167135604+amirpvishere@users.noreply.github.com>
2024-04-16 02:22:51 +0330

    Add files via upload

---
+++ Password Validation.py
@@ -0,0 +1,16 @@
+def password_validation(password):
+    if len(password) < 8:
+        print('Your password must contain at least 8 character')
+    elif password.isnumeric():
+        print('Your password must contain at least one character')
+    elif password.isalpha():
+        print('Your password must contain at least one number')
+    elif password.islower():
+        print('Your password must contain at least one upper letter')
+    else:
+        print('Your password has been created')
+
+
+while True:
+    password = input('Enter your password: ')
+    password_validation(password)
